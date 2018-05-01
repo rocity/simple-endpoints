9249f51ac1e3bda15f952154c237db2ac3d9b50f
kkalis <kevin@swiftkind.com>
2018-05-01 09:43:01 +0800

    Add cors

--- animals/settings.py
+++ animals/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'rest_framework',
     'breeds',
 ]
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,3 +126,4 @@ STATIC_ROOT = path.join(BASE_DIR, 'statics')
 MEDIA_URL = config('MEDIA_URL', default='/m/')
 MEDIA_ROOT = path.join(BASE_DIR, 'medias')
 
+CORS_ORIGIN_ALLOW_ALL = True
