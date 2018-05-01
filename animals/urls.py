e68a981ec6f613b83ee63b5c0fb4f8da7d2a51b2
kkalis <kevin@swiftkind.com>
2018-05-01 10:48:41 +0800

    Remove list

--- animals/urls.py
+++ animals/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ] + router.urls
 
-urlpatterns.extend([
+urlpatterns.extend(
     static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +
     static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-])
+)
